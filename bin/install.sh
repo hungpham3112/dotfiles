5ab243bdd0846dd458699185108684bc9d9d9089
sofia <sofia@pop-os.localdomain>
2023-01-05 23:03:51 +0700

    Fix some colorscheme error and change ~ -> /home/sofia

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOTFILES_DIR=~/.dotfiles/
-CONFIG_DIR=~/.config/
+DOTFILES_DIR="$HOME/.dotfiles/"
+CONFIG_DIR="$HOME/.config/"
 GREEN='\033[0;32m' # Green Color
 RED='\033[0;31m' # Red Color
 NC='\033[0m' # No Color
@@ -32,11 +32,11 @@ function update_system() {
 
 function install_curl() {
     if command -v curl > /dev/null; then
-        echo -e "${GREEN}Curl is installed ${CHECK_DONE}"
+        echo -e "${GREEN}Curl is installed ${CHECK_DONE}${NC}"
     else
-        echo -e "${GREEN}Curl is not installed. Installing curl..."
+        echo -e "${GREEN}Curl is not installed. Installing curl...${NC}"
         sudo apt install curl
-        echo -e "${GREEN}Install curl done ${CHECK_DONE}"
+        echo -e "${GREEN}Install curl done ${CHECK_DONE}${NC}"
     fi
 }
 
@@ -45,61 +45,67 @@ function install_vide() {
 }
 
 function clone_dotfiles() {
-    if [ -d "/path/to/folder" ]; then
-      rm -r "/path/to/folder"
+    if [[ -d "$DOTFILES_DIR" ]]; then
+      sudo rm -rf $DOTFILES_DIR
     fi
     if sudo git clone https://github.com/hungpham3112/.dotfiles.git; then
-        echo -e "${GREEN}Clone dotfiles folder successfully${CHECK_DONE}"
+        echo -e "${GREEN}Clone dotfiles folder successfully ${CHECK_DONE}${NC}"
     else
-        echo -e "${RED}Clone dotfiles folder fail"
+        echo -e "${RED}Clone dotfiles folder fail ${NC}"
     fi
 }
 
 function load_gnome_shell_settings() {
     # dconf dump /org/gnome/terminal/ > $DOTFILES_DIR
-    if cat $DOTFILES_DIR/gterminal.preferences | dconf load /org/gnome/terminal/legacy/profiles:/; then
-        echo -e "${GREEN}Loading gnome shell settings successfully${CHECK_DONE}"
+    if cat "${DOTFILES_DIR}/gterminal.preferences" | dconf load /org/gnome/terminal/legacy/profiles:/; then
+        echo -e "${GREEN}Loading gnome shell settings successfully ${CHECK_DONE}${NC}"
     else
-        echo -e "${RED}Loading gnome shell settings fail"
+        echo -e "${RED}Loading gnome shell settings fail ${NC}"
     fi
 }
 
 function load_autostart_program() {
-    SWAPKEY_FILE=$CONFIG_DIR/autostart/swapkey.desktop
-    if [[ -f $SWAPKEY_FILE ]]
+    SWAPKEY_FILE="${CONFIG_DIR}/autostart/swapkey.desktop"
+    if [[ -f "$SWAPKEY_FILE" ]]
     then
-        rm $SWAPKEY_FILE -f
-        sudo cp $DOTFILES_DIR/swapkey.desktop $SWAPKEY_FILE
-        echo -e "${GREEN}Copy swapkey autostart file successfully ${CHECK_DONE}"
+        sudo rm $SWAPKEY_FILE -f
+        sudo cp "${DOTFILES_DIR}/swapkey.desktop" $SWAPKEY_FILE
+        echo -e "${GREEN}Copy swapkey autostart file successfully ${CHECK_DONE}${NC}"
     else
-        sudo cp $DOTFILES_DIR/swapkey.desktop $SWAPKEY_FILE
-        echo -e "${RED}Copy swapkey autostart file fail"
+        sudo cp "${DOTFILES_DIR}/swapkey.desktop" $SWAPKEY_FILE
+        echo -e "${GREEN}Copy swapkey autostart file successfully ${CHECK_DONE}${NC}"
     fi
 }
 
 function install_themes() {
-    THEME_DIR=/usr/share/themes/
+    THEME_DIR="/usr/share/themes/Sweet/"
+    if [[ -d $THEME_DIR ]]; then
+      sudo rm -rf $THEME_DIR
+    fi
     if sudo git clone --single-branch --branch nova https://github.com/EliverLara/Sweet.git $THEME_DIR; then
-        echo -e "${GREEN}Install theme successfully ${CHECK_DONE}"
+        echo -e "${GREEN}Install theme successfully ${CHECK_DONE}${NC}"
     else
-        echo -e "${RED}Install theme fail "
+        echo -e "${RED}Install theme fail ${NC}"
     fi
 }
 
 function install_icons() {
-    ICON_DIR=/usr/share/icons/
+    ICON_DIR="/usr/share/icons/candy-icons/"
+    if [[ -d $ICON_DIR ]]; then
+      sudo rm -rf $ICON_DIR
+    fi
     if sudo git clone https://github.com/EliverLara/candy-icons.git $ICON_DIR; then
-        echo -e "${GREEN}Install icon successfully ${CHECK_DONE}"
+        echo -e "${GREEN}Install icon successfully ${CHECK_DONE}${NC}"
     else
-        echo -e "${RED}Install icon fail "
+        echo -e "${RED}Install icon fail ${NC}"
     fi
 }
 
 function copy_xmodmap() {
-    if sudo cp $DOTFILES_DIR/.Xmodmap $CONFIG_DIR/.Xmodmap; then
-        echo -e "${GREEN}Copy .Xmodmap file successfully ${CHECK_DONE}"
+    if sudo cp "${DOTFILES_DIR}/.Xmodmap" "${CONFIG_DIR}/.Xmodmap"; then
+        echo -e "${GREEN}Copy .Xmodmap file successfully ${CHECK_DONE}${NC}"
     else
-        echo -e "${RED}Copy .Xmodmap file fail"
+        echo -e "${RED}Copy .Xmodmap file fail ${NC}"
     fi
 }
 
